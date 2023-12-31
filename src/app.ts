1a3d2d204be24f2623a5c9075ecfaf607062837d
Facundo <facuhannoch@gmail.com>
2023-12-30 21:53:28 -0300

    test 4

--- src/app.ts
+++ src/app.ts
@@ -47,8 +47,8 @@ app.use(express.json());
 // const storage = multer.memoryStorage();
 // const upload = multer({ storage: storage });
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 const writeFileAsync = promisify(fs.writeFile);
 const unlinkAsync = promisify(fs.unlink);
